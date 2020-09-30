aeb2de370d233fdff11f4baf1f1eada4dcdf5737
Angus Wai <anguswai1999@gmail.com>
2020-09-30 11:55:49 -0400

    added test implementation in main.cpp

--- OOP3200-F2020-Lesson4/Main.cpp
+++ OOP3200-F2020-Lesson4/Main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+#include "Person.h"
+#include "Professor.h"
+#include "Student.h"
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Person person = Person("Bob", "Smith", 35.6f);
+	person.SaysHello();
+
+	std::cout << person.ToString() << std::endl;
+
+	Student student = Student("Mike", "Jones", 22.0f, "MJ123456789");
+	student.SaysHello();
+	student.Studies();
+
+	std::cout << student.ToString() << std::endl;
+
+	Professor professor = Professor("Andrew", "Hogue", 38.0f, "AH123456789");
+	professor.SaysHello();
+	professor.Teaches();
+
+	std::cout << professor.ToString() << std::endl;
+
 }
